111623870ac26ceeac86113a80e01d76190c7a1c
Kizebe <kizebe.info@gmail.com>
2026-03-31 22:06:48 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import '@/app/globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'NEZU - 5 Pillér Életmód Webshop',
+  title: 'NEZU - Az Életmód Webshop',
   description: 'Prémium Whiskey, BBQ, Kids, Plastic és Pet termékek egyenesen Yiwu-ból.',
 }
 
